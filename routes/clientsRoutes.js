c4c2d5f9a5cf0b7b9d07a86c82e63b2b92bdb6e1
David Carrera <142541347+david-carrerx@users.noreply.github.com>
2025-05-13 15:17:54 -0600

    Ruta de actualizar un cliente

--- routes/clientsRoutes.js
+++ routes/clientsRoutes.js
@@ -26,4 +26,41 @@ router.post('/add-client', async (req, res) => {
     }
 });
 
+router.get('/clients', async (req, res) => {
+    try {
+        const clients = await Client.find();
+        res.status(200).json(clients);
+    } catch (e) {
+        res.status(500).json({msg: "Server error"});
+    }
+});
+
+router.put('/update-client/:id', async (req, res) => {
+    try {
+        const { start_date, end_date, package } = req.body;
+
+        if(!start_date || !end_date || !package){
+            return res.status(400).json({ msg: "All fields are required"});
+        }
+
+        const updatedClient = await Client.findByIdAndUpdate(
+            req.params.id,{
+                start_date: new Date(start_date),
+                end_date: new Date(end_date),
+                package
+            },
+            { new: true }
+        );
+
+        if(!updatedClient){
+            return res(404).json({ msg: "Client not found"});
+        }
+
+        res.status(200).json({ msg: "Client updated successfully"});
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ msg: "Server error"});
+    }
+});
+
 module.exports = router;
